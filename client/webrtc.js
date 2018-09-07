70e4912208685c185c176b6b26cf8b51076fa8d4
Mohammad Alam <Mohammad.Alam@blinemedical.com>
2018-09-07 11:23:38 -0400

    some more improvements

--- client/webrtc.js
+++ client/webrtc.js
@@ -2,53 +2,69 @@ let localVideo;
 let localStream;
 let remoteVideo;
 let remoteVideo2;
-let peerConnection;
+let remoteVideo3;
+let remoteVideo4;
+//let peerConnection;
 let uuid;
 let serverConnection;
 let captureInputs = [];
 
-var socket = io.connect();
-socket.on('connect', function(data) {
+
+let socket = io.connect();
+socket.on('connect', function (data) {
     socket.emit('client', JSON.stringify({'msgId': 100, 'uuid': uuid, 'data': ''}));
 });
 
-socket.on('CaptureNodeConfigResponse', function(data) {
-    //console.log('CaptureNodeConfigResponse ', data);
+socket.on('CaptureNodeConfigResponse', function (data) {
     captureInputs = [];
     let captureNode = JSON.parse(data);
-    _.forEach(captureNode.captureInputs, function(ci) {
-        _.forEach(ci.channels, function(ch) {
+    _forEach(captureNode.captureInputs, function (ci) {
+        _forEach(ci.channels, function (ch) {
             captureInputs.push({'captureInputId': ci.captureInputId, 'channelId': ch.channelId})
         });
     });
     document.getElementById("captureInputs").innerHTML = JSON.stringify(captureInputs);
 });
 
-socket.on('offer', function(data) {
-    console.log('offer ', data);
+
+function findPeerFromUuid(peerid) {
+    let peer;
+    peer = _filter(connections, (c) => {
+        return c.peerid === peerid
+    });
+    return peer[0];
+}
+
+socket.on('offer', function (data) {
     let offer = JSON.parse(data);
-    console.log('offer.peerId ', offer.peerId);
-    console.log('offer.sdp ', offer.sdp);
-    if (offer.peerId === uuid)
-    {
+    console.log('offer ', offer);
+
+    let peer = findPeerFromUuid(offer.peerId);
+    if (!_isNil(peer)) {
         let sdp = {'type': "offer", 'sdp': offer.sdp};
         console.log('sdp ', sdp);
-        peerConnection.setRemoteDescription(new RTCSessionDescription(sdp)).then(function () {
+        peer.peerConnection.setRemoteDescription(new RTCSessionDescription(sdp)).then(function () {
             // Only create answers in response to offers
-            peerConnection.createAnswer().then(createdDescription).catch(errorHandler);
-
+            peer.peerConnection.createAnswer().then(function (description) {
+                createdDescription(peer, description)
+            }).catch(errorHandler);
         }).catch(errorHandler);
+    }
+    else {
+        console.log('cannot find peerId ', offer.peerId);
     }
 });
 
-socket.on('iceCandidate', function(data) {
-    console.log('iceCandidate ', data);
-
+socket.on('iceCandidate', function (data) {
     let candidate = JSON.parse(data);
-    console.log('candidate.ice ', candidate.ice);
-    //if (offer.peerId === uuid)
-    {
-        peerConnection.addIceCandidate(new RTCIceCandidate(candidate.ice)).catch(errorHandler);
+    console.log('Received ICE candidate ', candidate.ice);
+
+    let peer = findPeerFromUuid(candidate.peerId);
+    if (!_isNil(peer)) {
+        peer.peerConnection.addIceCandidate(new RTCIceCandidate(candidate.ice)).catch(errorHandler);
+    }
+    else {
+        console.log('Cannot find peerId ', candidate.peerId);
     }
 });
 
@@ -64,10 +80,8 @@ function pageReady() {
     localVideo = document.getElementById('localVideo');
     remoteVideo = document.getElementById('remoteVideo');
     remoteVideo2 = document.getElementById('remoteVideo2');
-
-/*    serverConnection = new WebSocket('ws://' + window.location.hostname + ':8002' + '/captureNode');
-    serverConnection.onmessage = gotMessageFromServer;*/
-
+    remoteVideo3 = document.getElementById('remoteVideo3');
+    remoteVideo4 = document.getElementById('remoteVideo4');
 
     let constraints = {
         video: true,
@@ -90,22 +104,60 @@ async function configure() {
     socket.emit('client', JSON.stringify({msgId: 101, data: ""}));
 }
 
-function start(isCaller) {
-    peerConnection = new RTCPeerConnection(peerConnectionConfig);
-    peerConnection.onicecandidate = gotIceCandidate;
+function start(index) {
+    let ci = captureInputs[index];
+    if (!_isNil(ci)) {
+        console.log('Starting WebRTC connection for captureInput ', ci.captureInputId);
+        startWebRTCConnection(ci);
+    }
+}
+
+function startAll(isCaller) {
+    _forEach(captureInputs, (ci) => {
+        console.log('Starting WebRTC connection for captureInput ', ci.captureInputId);
+        startWebRTCConnection(ci);
+    })
+}
+
+let connections = [];
+
+function startWebRTCConnection(captureInput) {
+    let peerid = createUUID();//uuid + captureInput.captureInputId;
+    let peerConnection = new RTCPeerConnection(peerConnectionConfig);
+    peerConnection.onicecandidate = function (event) {
+        gotIceCandidate(event, peerid);
+    };
     peerConnection.ontrack = gotRemoteStream;
+
+    let peer = {
+        peerid: peerid,
+        peerConnection: peerConnection,
+        captureInputId: captureInput.captureInputId,
+        channelId: captureInput.channelId
+    };
+    connections.push(peer);
+
     //peerConnection.addStream(localStream);
 
     console.log('sending get sdp message');
-    //serverConnection.send(JSON.stringify({'msgId': 100, 'uuid': uuid}));
 
     //msgId: 102 = getSDP
-    let webRTCSignallingMessage = {'peerId': uuid,
-        'captureInputId': captureInputs[0].captureInputId,
-        'channelId': captureInputs[0].channelId,
-        'jsonMessage': JSON.stringify({'msgId': 102, 'uuid': uuid, 'message': ''})
+
+    let webRTCSignallingMessage = {
+        'peerId': peerid,
+        'captureInputId': captureInput.captureInputId,
+        'channelId': captureInput.channelId,
+        'jsonMessage': JSON.stringify({'msgId': 102, 'uuid': peerid, 'message': ''})
     };
-    socket.emit('client', JSON.stringify({'msgId': 102, 'uuid': uuid, 'data': JSON.stringify(webRTCSignallingMessage)}));
+    let message = JSON.stringify({
+        'msgId': 102,
+        'peerId': peerid,
+        'data': JSON.stringify(webRTCSignallingMessage)
+    });
+
+    socket.emit('client', message);
+    console.log('Sending get sdp message ', message);
+
     /*  if(isCaller) {
         peerConnection.createOffer().then(createdDescription).catch(errorHandler);
       }*/
@@ -133,53 +185,78 @@ function gotMessageFromServer(message) {
             console.log('Unknown MsgId ', signal.msgId);
         }
     }
-    catch (ex)
-    {
+    catch (ex) {
 
-        console.log('Cannot handle incoming message ',ex);
+        console.log('Cannot handle incoming message ', ex);
 
     }
 }
 
-function gotIceCandidate(event) {
+function gotIceCandidate(event, peerId) {
+    let peer = findPeerFromUuid(peerId);
     if (event.candidate != null) {
-        console.log('sending ice candidate ');
-        //serverConnection.send(JSON.stringify({'ice': event.candidate, 'uuid': uuid}));
-        //serverConnection.send(JSON.stringify({'msgId': 103, 'message': event.candidate}));
-        let webRTCSignallingMessage = {'peerId': uuid,
-            'captureInputId': captureInputs[0].captureInputId,
-            'channelId': captureInputs[0].channelId,
-            'jsonMessage': JSON.stringify({'msgId': 104, 'uuid': uuid, 'message': event.candidate})
+        let webRTCSignallingMessage = {
+            'peerId': peerId,
+            'captureInputId': peer.captureInputId,
+            'channelId': peer.channelId,
+            'jsonMessage': JSON.stringify({'msgId': 104, 'uuid': peerId, 'message': event.candidate})
         };
-        socket.emit('client', JSON.stringify({'msgId': 104, 'uuid': uuid, 'data': JSON.stringify(webRTCSignallingMessage)}));
+        console.log('sending ice candidate ', webRTCSignallingMessage);
+        socket.emit('client', JSON.stringify({
+            'msgId': 104,
+            'uuid': peerId,
+            'data': JSON.stringify(webRTCSignallingMessage)
+        }));
     }
 }
 
-function createdDescription(description) {
+function createdDescription(peer, description) {
     console.log('got description');
 
-    peerConnection.setLocalDescription(description).then(function () {
-        console.log('sending answer ', peerConnection.localDescription);
-        //serverConnection.send(JSON.stringify({'msgId': 102, 'message': peerConnection.localDescription, 'uuid': uuid}));
-        let webRTCSignallingMessage = {'peerId': uuid,
-            'captureInputId': captureInputs[0].captureInputId,
-            'channelId': captureInputs[0].channelId,
-            'jsonMessage': JSON.stringify({'msgId': 105, 'uuid': uuid, 'message': peerConnection.localDescription})
+    peer.peerConnection.setLocalDescription(description).then(function () {
+        console.log('sending answer ', peer.peerConnection.localDescription);
+
+        let webRTCSignallingMessage = {
+            'peerId': peer.peerid,
+            'captureInputId': peer.captureInputId,
+            'channelId': peer.channelId,
+            'jsonMessage': JSON.stringify({
+                'msgId': 105,
+                'uuid': peer.peerid,
+                'message': peer.peerConnection.localDescription
+            })
         };
-        socket.emit('client', JSON.stringify({'msgId': 105, 'uuid': uuid, 'data': JSON.stringify(webRTCSignallingMessage)}));
+        socket.emit('client', JSON.stringify({
+            'msgId': 105,
+            'uuid': peer.peerid,
+            'data': JSON.stringify(webRTCSignallingMessage)
+        }));
     }).catch(errorHandler);
 }
 
 let remoteVideoCount = 0;
+
 function gotRemoteStream(event) {
-    console.log('got remote stream');
+    //little dumb; but it is fine for now
+    console.log('got remote stream ');
     if (remoteVideoCount === 0) {
         remoteVideo.srcObject = event.streams[0];
-        remoteVideoCount++;
     }
-    else {
+    else if (remoteVideoCount === 1){
         remoteVideo2.srcObject = event.streams[0];
     }
+    else if (remoteVideoCount === 2)
+    {
+        remoteVideo3.srcObject = event.streams[0];
+    }
+    else if (remoteVideoCount === 3)
+    {
+        remoteVideo4.srcObject = event.streams[0];
+    }
+    else {
+        console.log('No place to view stream');
+    }
+    remoteVideoCount++;
 }
 
 function errorHandler(error) {
@@ -207,3 +284,42 @@ function createUUID() {
 //101: offer
 //102 answer
 //103 ice candidate
+
+
+/////////////////////////// lodash functions /////////////////
+function _forEach(collection, iteratee) {
+    const func = Array.isArray(collection) ? _arrayEach : baseEach
+    return func(collection, iteratee)
+}
+
+function _filter(array, predicate) {
+    let index = -1
+    let resIndex = 0
+    const length = array == null ? 0 : array.length
+    const result = []
+
+    while (++index < length) {
+        const value = array[index]
+        if (predicate(value, index, array)) {
+            result[resIndex++] = value
+        }
+    }
+    return result
+}
+
+function _isNil(value) {
+    return value == null
+}
+
+
+function _arrayEach(array, iteratee) {
+    let index = -1
+    const length = array == null ? 0 : array.length
+
+    while (++index < length) {
+        if (iteratee(array[index], index, array) === false) {
+            break
+        }
+    }
+    return array
+}
